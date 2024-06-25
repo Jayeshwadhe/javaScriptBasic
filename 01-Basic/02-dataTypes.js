f411d6c80008857e30f35e4eb7efadc88d9526b0
jayesh wadhe <jayeshwadhe08@gmai.com>
2024-06-25 18:05:34 +0530

    datatypes

---
+++ 01-Basic/02-dataTypes.js
@@ -0,0 +1,8 @@
+"Use Strict"; // Treats all JS code as newer version;
+
+// alert (3 + 3); //we are not using node.js, not browser;
+
+let stage = null; // null typeof is object;
+
+console.log(stage);
+console.log(typeof stage)
--- 01-Basic/test.js
+++ 01-Basic/test.js
@@ -1,1 +1,17 @@
-console.log("javaScript")
+const accountId  = "abc123";
+let accountEmail = "jayesh@yahoo.com";
+var accountpassword = "12345";
+accountCity = "Burhnapur"
+
+//accountId = "123" => Not allowed to update const value.
+accountEmail = "hdd@yahoo.com",
+accountpassword = "jaishreeram",
+accountCity = "vapi"
+
+console.log(accountCity)
+/*
+Prefer not to use var in production code
+because from that it will create issue in block scope.
+*/
+
+console.table([accountId,accountEmail, accountpassword, accountCity])
