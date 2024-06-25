7a0168fc2150aa533c25fbc33ac66d72c0911368
jayesh wadhe <jayeshwadhe08@gmai.com>
2024-06-25 18:27:07 +0530

    convertion of datatypes

---
+++ 01-Basic/03-convertionOperation.js
@@ -0,0 +1,15 @@
+let age = "35dfd";
+
+let valueInNumber = Number(age)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//35 = 35
+//35dfd = NaN
+
+let login = 1
+
+let valueofBoolean = Boolean(login)
+
+console.log(valueofBoolean);
+console.log(typeof valueofBoolean);
