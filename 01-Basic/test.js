7738433a43702d5575dd29f400ca544066128f25
Jayesh wadhe <80095765+Jayeshwadhe@users.noreply.github.com>
2025-12-03 08:47:09 +0000

    container config

--- 01-Basic/test.js
+++ 01-Basic/test.js
@@ -1,17 +1,1 @@
-const accountId = 12345
-let accountEmail = "jayesh@google.com";
-var accountName =  "jayeshWadhe";
-accountCity = "burhanpur";
-
-//accountId = 2345
-accountEmail = "wadhe@gmail.com";
-accountName = "wadheJayesh";
-accountCity = "Indore";
-
-console.log(accountId);
-/*
-Prefer not to use var in production code
-because from that it will create issue in block scope.
-*/
-
-console.table([accountId, accountEmail, accountName, accountCity])
+console.log("Jayesh")
